af78fef7e0e690dc015abb8bc9aa24bfe9fc5ed7
Петрикин Иван Витальевич <i.petrikin@serv-en.local>
2025-08-11 14:28:25 +0500

    Prepare for Render deployment

--- working_service_log_project/config/settings.py
+++ working_service_log_project/config/settings.py
@@ -6,9 +6,9 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'service-application-key'
-DEBUG = True
-ALLOWED_HOSTS = ['*']
+SECRET_KEY = os.getenv('SECRET_KEY','service-application-key')
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
